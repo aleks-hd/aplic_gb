5fabc2ae189e795281f4a4c0dcbd8c7596cbe4ef
aleks-hd2015 <aleks-hd2015@yandex.ru>
2021-06-13 23:07:25 +0400

    Создание Второй активити

--- app/src/main/java/com/hfad/aplic_gb/MainActivity.java
+++ app/src/main/java/com/hfad/aplic_gb/MainActivity.java
@@ -1,19 +1,32 @@
 package com.hfad.aplic_gb;
 
 /*Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton.
-Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты. 2. Создать ещё один макет
-(если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number,
-textPassword, textEmailAddress и другие значения. 3. Добавить в проект элемент CalendarView. 4. * Разобраться, что такое параметр*/
+Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты.
+2. Создать ещё один макет(если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number,
+textPassword, textEmailAddress и другие значения.
+3. Добавить в проект элемент CalendarView. 4. * Разобраться, что такое параметр*/
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewHeader);
+        editText = findViewById(R.id.editTextHeader);
+        btn = findViewById(R.id.button);
+
     }
 }
---
+++ app/src/main/java/com/hfad/aplic_gb/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.hfad.aplic_gb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+}
