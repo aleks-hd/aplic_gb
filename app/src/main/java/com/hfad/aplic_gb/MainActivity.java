66906aa067f632a685e0f6872e6ba2d03087865e
aleks-hd2015 <aleks-hd2015@yandex.ru>
2021-06-13 22:35:17 +0400

    Создание View

--- app/src/main/java/com/hfad/aplic_gb/MainActivity.java
+++ app/src/main/java/com/hfad/aplic_gb/MainActivity.java
@@ -1,5 +1,10 @@
 package com.hfad.aplic_gb;
 
+/*Создать проект со следующими пользовательскими элементами: TextView, EditText, Button, Switch, CheckBox, ToggleButton.
+Для работы использовать LinearLayout. Использовать различные шрифты, цвета, размеры, прочие атрибуты. 2. Создать ещё один макет
+(если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number,
+textPassword, textEmailAddress и другие значения. 3. Добавить в проект элемент CalendarView. 4. * Разобраться, что такое параметр*/
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
